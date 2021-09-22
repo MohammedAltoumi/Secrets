25c4e7c0a1874886f7e4d018b9a64aff744b8e55
unknown <khalid.tu21@gmail.com>
2021-09-22 11:09:37 -0400

    level-3 of authentication project

---
+++ app.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended : true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true, useUnifiedTopology: true}).
+then( () => {
+    console.log('connected seccessfully')
+}).catch(() => {
+  console.log('something went wrong');
+})
+
+const userSchema = new mongoose.Schema ({
+  email : String,
+  password : String
+})
+
+const secret =process.env.SECRET;
+
+userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password']})
+
+const User = mongoose.model("User", userSchema);
+
+
+app.route("/")
+    .get((req, res) => {
+        res.render("home")
+    })
+
+app.route("/login")
+    .get((req, res) => {
+        res.render("login")
+    })
+    .post((req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email:username}, (err, foundUser) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser.password===password) {
+                    res.render("secrets")
+                }
+            }
+        })
+    })
+    
+app.route("/register")
+    .get((req, res) => {
+        res.render("register")
+    })
+    .post((req, res) => {
+        const newUser = User({
+            email : req.body.username,
+            password : req.body.password
+        })
+        newUser.save();
+        res.render("secrets")
+    })
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000.");
+})
+
