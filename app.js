eef9eb3ebd6e3b4b66f57f46f86b2a586667586e
unknown <khalid.tu21@gmail.com>
2021-09-22 12:29:26 -0400

    level-4 of authentication project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
 const app = express();
 
@@ -22,9 +22,6 @@ const userSchema = new mongoose.Schema ({
   password : String
 })
 
-const secret =process.env.SECRET;
-
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password']})
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +37,7 @@ app.route("/login")
     })
     .post((req, res) => {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne({email:username}, (err, foundUser) => {
             if (err) {
@@ -60,7 +57,7 @@ app.route("/register")
     .post((req, res) => {
         const newUser = User({
             email : req.body.username,
-            password : req.body.password
+            password : md5(req.body.password)
         })
         newUser.save();
         res.render("secrets")
